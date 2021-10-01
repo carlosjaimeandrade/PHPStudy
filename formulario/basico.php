3e5f531f576ec5273dad2fb47cd0e5527f2e1936
Carlos Jaime <jaime_andrek@hotmail.com>
2021-10-01 19:58:17 -0300

    Formulario validação

--- formulario/basico.php
+++ formulario/basico.php
@@ -2,36 +2,63 @@
 <h1>Cadastro</h1>
 <!-- cdn -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-<form action="#">
+<form action="#" method="post">
     <div class="form-row">
         <div class="form-group col-md-6">
             <label for="nome">Nome</label>
-            <input class="form-control" id='name' type="text" name='name' placeholder="Nome" required>
+            <input class="form-control" id='name' type="text" name='name' placeholder="Nome" >
         </div>
         <div class="form-group col-md-6">
-            <label for="data">Data de nacimento</label>
-            <input class="form-control" id='data' type="date" name='data' placeholder="data" required>
+            <label for="nascimento">Data de nacimento</label>
+            <input class="form-control" id='nascimento' type='text' name='nascimento' placeholder="nascimento" >
         </div>
     </div>
     <div class="form-row">
         <div class="form-group col-md-6">
             <label for="email">Email</label>
-            <input class="form-control" id='email' type="email" name='email' placeholder="email" required>
+            <input class="form-control" id='email' type="text" name='email' placeholder="email" >
         </div>
         <div class="form-group col-md-6">
             <label for="site">Site</label>
-            <input class="form-control" id='site' type="site" name='text' placeholder="Nome" required>
+            <input class="form-control" id='site' type="text" name='site' placeholder="Nome" >
         </div>
     </div>
     <div class="form-row">
         <div class="form-group col-md-6">
             <label for="filhos">Quantidade de filhos</label>
-            <input class="form-control" id='filhos' type="filhos" name='text' placeholder="filhos" required>
+            <input class="form-control" id='filhos' type="namber" name='filhos' placeholder="filhos" >
         </div>
         <div class="form-group col-md-6">
             <label for="salario">Salário</label>
-            <input class="form-control" id='salario' type="salario" name='text' placeholder="salario" required>
+            <input class="form-control" id='salario' type="salario" name='text' placeholder="salario" >
         </div>
     </div>
     <button class="btn btn-primary" >Enviar</button>
 </form>
+
+<?php
+if(count($_POST)>0){
+    if(!filter_input(INPUT_POST, "nome")){
+        echo "Nome é obrigatorio" . "<br>";
+    }
+
+    if(filter_input(INPUT_POST, "nascimento")){
+        $data = DateTime::createFromFormat('d/m/Y', $_POST['nascimento']);
+        if(!$data){
+            echo "Data deve ser estar no formado padrão dd/mm/aaaa". "<br>";
+        }
+    }
+}
+
+
+if(!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
+    echo "Email invalido" . "<br>";;
+}
+
+if(!filter_var($_POST['site'], FILTER_VALIDATE_URL)){
+    echo "site invalido";
+}
+
+
+
+?>
