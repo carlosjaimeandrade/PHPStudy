3d99e8ee128f4d62584409bafa917434422c1258
Carlos jaime <jaime_andrek@hotmail.com>
2021-12-05 17:15:17 -0300

    update

--- anotacao/htmlenti.php
+++ anotacao/htmlenti.php
@@ -2,4 +2,14 @@
 $exemplo = "<script>alert('virus')</script>";
 $exemplo = strip_tags($exemplo);
 echo $exemplo;
+$str = "A 'quote' is <b>bold</b>";
+
+$str = "A 'quote' is <b>bold</b>";
+
+// Outputs: A 'quote' is &lt;b&gt;bold&lt;/b&gt;
+echo htmlentities($str);
+echo "<br>";
+// Outputs: A &#039;quote&#039; is &lt;b&gt;bold&lt;/b&gt;
+echo htmlentities($str, ENT_NOQUOTES);
 ?>
+
