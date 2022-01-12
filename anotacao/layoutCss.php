d39dac9c40a71e26ae518406b7da0e17b8357dce
Carlos jaime <jaime_andrek@hotmail.com>
2022-01-12 17:01:39 -0300

    Ajuste layout css filmes

--- anotacao/layoutCss.php
+++ anotacao/layoutCss.php
@@ -9,63 +9,120 @@
 </head>
 
 <body>
-    <div class="container">
-        <div class='imagem'>
-            <img class="imagemedit" src="filme2.jpg" alt="">
-            <div class="front-color"></div>
-            <div class="action"><button>ASSISTA AGORA</button></div>
-        </div>
-
-        <div class='imagem'>
-            <img class="imagemedit" src="transferir.jpg" alt="">
-            <div class="front-color"></div>
-            <div class="action"><button>ASSISTA AGORA</button></div>
-        </div>
+    <div class="descricao-filmes">
 
     </div>
 
+    <div class="container-pai">
+        <div class="container">
+            <div class='imagem'>
+                <img class="imagemedit" src="filme2.jpg" alt="">
+                <div class="front-color"></div>
+                <div class="action"><button>ASSISTA AGORA</button></div>
+            </div>
+
+            <div class='imagem'>
+                <img class="imagemedit" src="transferir.jpg" alt="">
+                <div class="front-color"></div>
+                <div class="action"><button>ASSISTA AGORA</button></div>
+            </div>
+
+            <div class='imagem'>
+                <img class="imagemedit" src="transferir.jpg" alt="">
+                <div class="front-color"></div>
+                <div class="action"><button>ASSISTA AGORA</button></div>
+            </div>
+
+            <div class='imagem'>
+                <img class="imagemedit" src="transferir.jpg" alt="">
+                <div class="front-color"></div>
+                <div class="action"><button>ASSISTA AGORA</button></div>
+            </div>
+
+            <div class='imagem'>
+                <img class="imagemedit" src="transferir.jpg" alt="">
+                <div class="front-color"></div>
+                <div class="action"><button>ASSISTA AGORA</button></div>
+            </div>
+
+            <div class='imagem'>
+                <img class="imagemedit" src="transferir.jpg" alt="">
+                <div class="front-color"></div>
+                <div class="action"><button>ASSISTA AGORA</button></div>
+            </div>
+        </div>
+
+        
+    </div>
+
+
+
+        
+    </div>
 </body>
 
 </html>
 
 <style>
-    .container{
+    .container-pai{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        
+    }
+    body {
+        background-color: black;
+        margin: 0px;
+    }
+
+    .container {
         display: flex;
         flex-wrap: wrap;
+        justify-content: center;
+
     }
-    .imagem img{
+
+    .imagem img {
         flex-shrink: 0;
         position: absolute;
         width: 100%;
         height: 250px;
-  
+
     }
+
     .imagem {
-        width: 200px;
+        width: 180px;
         height: 250px;
-/*         background-image: url(transferir.jpg);
-        background-repeat: no-repeat;
-        background-position: center;
-        background-size: 100%; */
+
         position: relative;
         margin: 10px;
         border-radius: 4px;
         box-shadow: 0px 1px 8px 0px black;
         overflow: hidden;
-       
+
+    }
+
+    .descricao-filmes {
+        width: 100%;
+        height: 350px;
+        background-image: url(wall.jpg);
+        background-repeat: no-repeat;
+        background-position: center;
+
+        position: relative;
     }
 
     .front-color {
         position: absolute;
         top: 0;
-        background-color: blue;
+        background-color: black;
         width: 100%;
         height: 100%;
         opacity: 0;
         transition: 1s;
     }
 
-    .action{
+    .action {
         position: absolute;
         width: 100%;
         height: 100%;
@@ -77,22 +134,24 @@
     }
 
     .imagem:hover .front-color {
-        opacity: 0.3;
-       
+        opacity: 0.5;
+
 
     }
-    .imagem:hover  .action{
+
+    .imagem:hover .action {
         opacity: 1;
     }
 
-    .imagemedit{
+    .imagemedit {
         transition: 1s;
     }
-    .imagem:hover .imagemedit{
+
+    .imagem:hover .imagemedit {
         transform: scale(1.1);
     }
 
-    .action button{
+    .action button {
         padding: 10px;
         border: none;
         color: black;
