074bce565e4412342d0761cc4ec8aac301c9b596
Carlos jaime <jaime_andrek@hotmail.com>
2022-01-13 21:09:07 -0300

    css grid input

--- anotacao/cssGrid1.php
+++ anotacao/cssGrid1.php
@@ -13,14 +13,12 @@
     <h1>CSS GRID</h1>
 </div>
 <div class="grid-container">
-  <div class="item1">1</div>
-  <div class="item2">2</div>
-  <div class="item3">3</div>  
-  <div class="item4">4</div>
-  <div class="item5">5</div>
-  <div class="item6">6</div>
-  <div class="item7">7</div>
-  <div class="item8">8</div>
+  <div class="item"><input type="text"></div>
+  <div class="item"><input type="text"></div>
+  <div class="item"><input type="text"></div>
+  <div class="item"><input type="text"></div>
+  <div class="item"><input type="text"></div>
+  <div class="item"><input type="text"></div>
 </div>
 </body>
 
@@ -29,18 +27,17 @@
 <style>
 .grid-container {
   display: grid;
-  grid-template-columns: repeat(auto-fill, 200px);
+  grid-template-columns: repeat(auto-fill, 350px);
   justify-content: center;
-  grid-gap: 10px;
+
   background-color: #2196F3;
-  padding: 10px;
+ 
 }
 
 .grid-container > div {
-  background-color: rgba(255, 255, 255, 0.8);
-  text-align: center;
-  padding: 20px 0;
-  font-size: 30px;
-    height: 300px;
+
+}
+.item input{
+    width: 100%;
 }
 </style>
